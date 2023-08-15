d0432cf2e12dafdade14c3946924fe0d24ebd4ef
VISHAL GUNTHA <96721397+VISiiitl25@users.noreply.github.com>
2023-08-15 14:41:19 +0530

    Time: 124 ms (63.01%) | Memory: 79.8 MB (26.52%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     vector<vector<string>>st;
     bool isPalindrome(std::string s) {
-    // Convert the string to lowercase
-    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
-
-    // Remove non-alphanumeric characters
-    s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char c) { return !std::isalnum(c); }), s.end());
-
-    // Compare the string with its reverse
     return s == std::string(s.rbegin(), s.rend());
 }
     void recur(int l,int n,string s,vector<string>&stt){
