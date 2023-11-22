2a31d2549cc221fa0807a282048df33187b8778a
VISHAL GUNTHA <96721397+VISiiitl25@users.noreply.github.com>
2023-11-22 10:54:13 +0530

    Time: 44 ms (29.57%) | Memory: 22.3 MB (23.33%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+         vector<int> ans;
+        int x= 1e4 + 1;
+        vector<pair<int, int>> sum[x];
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums[i].size(); j++) {
+                int su = i + j;
+                sum[su].push_back({i, nums[i][j]});
+            }
+        }
+
+        for (int i = 0; i < 1e4+1; i++) {
+            sort(sum[i].begin(), sum[i].end(), greater<pair<int, int>>());
+            if(i%2){
+                reverse(sum[i].begin(),sum[i].end());
+            }
+            for (auto p : sum[i]) {
+                ans.push_back(p.second);
+            }
+        }
+
+        return ans;
+    }
+};
